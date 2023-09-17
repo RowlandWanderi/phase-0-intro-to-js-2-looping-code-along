548fa919c8861c5cb0962a3ff07be1ca08929a6b
Rowland <rowland.wanderi@student.moringaschool.com>
2023-09-17 06:18:11 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"]
+const message="surprise"
+
+function writeCards( names,message){
+
+    const thankYouMessages = [];
+
+    for(let i=0;i< names.length;i++){
+        const name = names[i];
+        const thankYouMessage = `Thank you, ${name}, for the wonderful ${message} gift!`;
+    thankYouMessages.push(thankYouMessage);
+    }
+    return thankYouMessages;
+}console.log(`${writeCards(names,message)}`);
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
